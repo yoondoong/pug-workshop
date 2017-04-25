7336c18efacededd08ffbc1fe963cff51fd5ea1f
Eun Kyung Yoon <eun.kyung.yoon.19@dartmouth.edu>
2017-04-25 15:06:31 -0400

    workshop final

--- options.js
+++ options.js
@@ -4,43 +4,43 @@ exports.happy = true;
 
 exports.sitemap = [
   {
-    header: 'Category A',
+    header: 'Sekaino Owari',
     links: [
-      { name: 'Link A.1', url: '#' },
-      { name: 'Link A.2', url: '#' },
-      { name: 'Link A.3', url: '#' },
-      { name: 'Link A.4', url: '#' },
-      { name: 'Link A.5', url: '#' }
+      { name: 'Hey! Ho!', url: 'https://youtu.be/qfsr0S_QGOU' },
+      { name: 'Mr.Heartache', url: 'https://youtu.be/q9iozQDEO_U' },
+      { name: 'Anti-Hero', url: 'https://youtu.be/v9V5aByfeCM' },
+      { name: 'Rainbow Colored WAr', url: 'https://youtu.be/od8CNHDkIJA' },
+      { name: 'Phantom', url: 'https://youtu.be/H51Xj5aEgkA' }
     ],
   },
   {
-    header: 'Category B',
+    header: 'Snoopy',
     links: [
-      { name: 'Link B.1', url: '#' },
-      { name: 'Link B.2', url: '#' },
-      { name: 'Link B.3', url: '#' },
-      { name: 'Link B.4', url: '#' },
-      { name: 'Link B.5', url: '#' }
+      { name: 'Dancing', url: 'https://media.giphy.com/media/J93sVmfYBtsRi/giphy.gif' },
+      { name: 'Chilling', url: 'https://media.giphy.com/media/aeu60CPZd8zw4/giphy.gif' },
+      { name: 'Laughing', url: 'https://media.giphy.com/media/xT9DPzhNGA8MKjxwFG/giphy.gif' },
+      { name: 'Fashion', url: 'https://media.giphy.com/media/GczaN55diMqMU/giphy.gif' },
+      { name: 'Cupid', url: 'https://media.giphy.com/media/RZkdGiEGI7aY8/giphy.gif' }
     ],
   },
   {
-    header: 'Category C',
+    header: 'Hyukoh',
     links: [
-      { name: 'Link C.1', url: '#' },
-      { name: 'Link C.2', url: '#' },
-      { name: 'Link C.3', url: '#' },
-      { name: 'Link C.4', url: '#' },
-      { name: 'Link C.5', url: '#' }
+      { name: 'Tomboy', url: 'https://youtu.be/pC6tPEaAiYU' },
+      { name: 'Wanli', url: 'https://youtu.be/mmDD-QWlQpU' },
+      { name: 'Leather Jacket', url: 'https://youtu.be/LzUETvviKtA' },
+      { name: 'Gondry', url: 'https://youtu.be/u3RAU0T2RC4' },
+      { name: 'Hookah', url: 'https://youtu.be/VOpv43AYMlA' }
     ],
   },
   {
-    header: 'Category D',
+    header: 'Beenzino',
     links: [
-      { name: 'Link D.1', url: '#' },
-      { name: 'Link D.2', url: '#' },
-      { name: 'Link D.3', url: '#' },
-      { name: 'Link D.4', url: '#' },
-      { name: 'Link D.5', url: '#' }
+      { name: 'January', url: 'https://youtu.be/YuNKWy7nA6Q' },
+      { name: 'Break', url: 'https://youtu.be/gLQk5TE_6yc' },
+      { name: 'Aquaman', url: 'https://youtu.be/8nq32TQBwXI' },
+      { name: 'If I Die Tomorrow', url: 'https://youtu.be/Rrt2JWCTObc' },
+      { name: 'Time Travel', url: 'https://youtu.be/fNc6kDLUiV4' }
     ],
   },
 ];
